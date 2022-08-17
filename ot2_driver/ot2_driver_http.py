21ff3f87d43524176cbc3b3ece36f907fe8e055c
Kyle Hippe <kyle.hippe@gmail.com>
2022-08-17 15:01:15 -0500

    Execute method does not return until completed, and now returns the information generated about the protocol on the OT2

--- ot2_driver/ot2_driver_http.py
+++ ot2_driver/ot2_driver_http.py
@@ -1,13 +1,14 @@
 """Driver implemented using HTTP protocol supported by Opentrons"""
-from enum import Enum
 import subprocess
+import time
+from enum import Enum
 from pathlib import Path
 from typing import Dict, List, Optional, Tuple
 
 import requests
-from urllib3 import Retry
 import yaml
 from pydantic import BaseModel
+from urllib3 import Retry
 
 from ot2_driver.config import PathLike, parse_ot2_args
 from ot2_driver.protopiler.protopiler import ProtoPiler
@@ -22,9 +23,17 @@ class OT2_Config(BaseModel):
     version: Optional[int]
 
 
-class Status(Enum):
+class RobotStatus(Enum):
     IDLE = "idle"
     RUNNING = "running"
+
+
+class RunStatus(Enum):
+    IDLE = "idle"
+    RUNNING = "running"
+    FINISHING = "finishing"
+    SUCCEEDED = "succeeded"
+    FAILED = "failed"
 
 
 class OT2_Driver:
@@ -46,10 +55,16 @@ class OT2_Driver:
         """
         self.config: OT2_Config = config
         self.protopiler: ProtoPiler = ProtoPiler(
-            template_dir=(Path(__file__).parent.resolve() / "protopiler/protocol_templates")
+            template_dir=(
+                Path(__file__).parent.resolve() / "protopiler/protocol_templates"
+            )
         )
 
-        self.retry_strategy = Retry(total=retries, backoff_factor=retry_backoff, status_forcelist=retry_status_codes)
+        self.retry_strategy = Retry(
+            total=retries,
+            backoff_factor=retry_backoff,
+            status_forcelist=retry_status_codes,
+        )
 
     def compile_protocol(self, config_path, resource_file=None) -> Tuple[str, str]:
         """Compile the protocols via protopiler
@@ -69,9 +84,14 @@ class OT2_Driver:
             path to the protocol file and resource file
         """
         if ".py" not in str(config_path):
-            self.protopiler.load_config(config_path=config_path, resource_file=resource_file)
+            self.protopiler.load_config(
+                config_path=config_path, resource_file=resource_file
+            )
 
-            protocol_out_path, protocol_resource_file = self.protopiler.yaml_to_protocol(
+            (
+                protocol_out_path,
+                protocol_resource_file,
+            ) = self.protopiler.yaml_to_protocol(
                 config_path, resource_file=resource_file
             )
 
@@ -123,13 +143,76 @@ class OT2_Driver:
         Dict[str, Dict[str, str]]
             the json response from the OT2 execute command
         """
-        execute_url = f"http://{self.config.ip}:{self.config.port}/runs/{run_id}/actions"
+        execute_url = (
+            f"http://{self.config.ip}:{self.config.port}/runs/{run_id}/actions"
+        )
         headers = {"Opentrons-Version": "2"}
         execute_json = {"data": {"actionType": "play"}}
 
-        execute_run_resp = requests.post(url=execute_url, headers=headers, json=execute_json)
+        # TODO: do some error checking/handling on execute
+        execute_run_resp = requests.post(
+            url=execute_url, headers=headers, json=execute_json
+        )
+        if (
+            execute_run_resp.status_code != 201
+        ):  # this is the good respcode for this endpoint
+            print(f"Could not run play action on {run_id}")
+            print(execute_run_resp.json())
 
-        return execute_run_resp.json()
+        while self.check_run_status(run_id) not in {
+            RunStatus.FAILED,
+            RunStatus.SUCCEEDED,
+        }:
+            time.sleep(1)
+
+        return self.get_run(run_id)
+
+    def check_run_status(self, run_id) -> RunStatus:
+        """Checks the status of a run
+
+        Parameters
+        ----------
+        run_id : str
+            The run id, given by the opentrons API
+
+        Returns
+        -------
+        RunStatus
+            A enum of the current run status as reported by the ot2 (IDLE, RUNNING, FINISHING, FAILED, SUCCEEDED)
+        """
+        # check run
+        check_run_url = f"http://{self.config.ip}:{self.config.port}/runs/{run_id}"
+        headers = {"Opentrons-Version": "2"}
+
+        check_run_resp = requests.get(url=check_run_url, headers=headers)
+        if check_run_resp.status_code != 200:
+            print(f"Cannot check run {run_id}")
+        status = RunStatus(check_run_resp.json()["data"]["status"])
+
+        return status
+
+    def get_run(self, run_id) -> Dict:
+        """Get the OT2 summary of a specific run
+
+        Parameters
+        ----------
+        run_id : str
+            The run id given by the OT2 api
+
+        Returns
+        -------
+        Dict
+            The response json dictionary
+        """
+        run_url = f"http://{self.config.ip}:{self.config.port}/runs/{run_id}"
+        headers = {"Opentrons-Version": "2"}
+
+        run_resp = requests.get(url=run_url, headers=headers)
+
+        if run_resp.status_code != 200:
+            print(f"Could not get run {run_id}")
+
+        return run_resp.json()
 
     def get_runs(self) -> Optional[List[Dict[str, str]]]:
         """Get all the runs currently stored on the ot2
@@ -147,13 +230,19 @@ class OT2_Driver:
         if runs_resp.status_code == 200:
             runs_simplified = []
             for run in runs_resp.json()["data"]:
-                runs_simplified.append({"runID": run["id"], "protocolID": run["protocolId"], "status": run["status"]})
+                runs_simplified.append(
+                    {
+                        "runID": run["id"],
+                        "protocolID": run["protocolId"],
+                        "status": run["status"],
+                    }
+                )
 
             return runs_simplified
 
         return None
 
-    def get_robot_status(self) -> Status:
+    def get_robot_status(self) -> RobotStatus:
         """Return the status of the robot currently.
 
         Returns
@@ -162,10 +251,10 @@ class OT2_Driver:
             Either IDLE or RUNNING
         """
         for run in self.get_runs():
-            if run["status"] == Status.RUNNING.value:
-                return Status.RUNNING
+            if run["status"] == RobotStatus.RUNNING.value:
+                return RobotStatus.RUNNING
 
-        return Status.IDLE
+        return RobotStatus.IDLE
 
     def send_request(self, request_extension: str, **kwargs) -> requests.Response:
         """Allows us to send arbitrary requests to the ot2 http server.
@@ -186,7 +275,9 @@ class OT2_Driver:
             If there is no `method` keyword argument, This method does not specify the http request method, user must provide as keyword argument
         """
         # sanitize preceeding '/'
-        request_extension = request_extension if "/" != request_extension[0] else request_extension[1:]
+        request_extension = (
+            request_extension if "/" != request_extension[0] else request_extension[1:]
+        )
         url = f"http://{self.config.ip}:{self.config.port}/{request_extension}"
 
         # check for headers
@@ -267,7 +358,9 @@ class OT2_Driver:
             run_id = run_resp.json()["data"]["id"]
 
         # queue the command
-        enqueue_payload = {"data": {"commandType": command, "params": params, "intent": intent}}
+        enqueue_payload = {
+            "data": {"commandType": command, "params": params, "intent": intent}
+        }
         enqueue_resp = requests.post(
             url=f"http://{self.config.ip}:{self.config.port}/runs/{run_id}/commands",
             headers=headers,
