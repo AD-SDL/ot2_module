57031cceb625f93586c3473bc322246d9e0101be
abestroka <abestroka@gmail.com>
2025-03-18 17:09:55 -0500

    Update ot2_node.py

--- src/ot2_node.py
+++ src/ot2_node.py
@@ -40,7 +40,7 @@ class OT2NodeConfig(RestNodeConfig):
     """name of node being used is required"""
     ip: str 
     "ip of opentrons device"
-    port: int 
+    ot2_port: int 
     "port of opentrons device"
 
 
